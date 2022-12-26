73290a9127da9403e15c96df3f822e1c1e186e69
Jon Mease <jonmmease@gmail.com>
2022-12-26 16:50:48 -0500

    another try

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,31 +1,14 @@
 declare -a _xtra_maturin_args
 
+# Set conda CC as custom CC in Rust
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
+export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
+export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
+
 if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] ; then
-
-      mkdir $SRC_DIR/.cargo
-      cat <<EOF >> $SRC_DIR/.cargo/config
-# Required for intermediate codegen stuff
-[target.x86_64-apple-darwin]
-linker = "$CC_FOR_BUILD"
-# Required for final binary artifacts for target
-[target.aarch64-apple-darwin]
-linker = "$CC"
-rustflags = [
-  "-C", "link-arg=-undefined",
-  "-C", "link-arg=dynamic_lookup",
-]
-
-EOF
-
-  _xtra_maturin_args+=(--target=aarch64-apple-darwin)
-
-  # This variable must be set to the directory containing the target's libpython DSO
-  export PYO3_CROSS_LIB_DIR=$PREFIX/lib
-
-  # xref: https://github.com/PyO3/pyo3/commit/7beb2720
-  export PYO3_PYTHON_VERSION=${PY_VER}
+  maturin build --release --strip --target=aarch64-apple-darwin
+else
+  maturin build --release --strip
 fi
-
-maturin build --release --strip --manylinux off "${_xtra_maturin_args[@]}"
 
 python -m pip install $SRC_DIR/target/wheels/vl_convert_python*.whl --no-deps -vv
