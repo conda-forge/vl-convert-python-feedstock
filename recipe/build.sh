73c3fa2b04c050fe1549d528c07f2a1fa46dfd57
Jon Mease <jonmmease@gmail.com>
2022-12-26 15:07:25 -0500

    remove interpreter arg (should use the current one)

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,6 @@ if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = 
   _xtra_maturin_args+=(--target=aarch64-apple-darwin)
 fi
 
-maturin build --release --strip --manylinux off --interpreter="${PYTHON}" "${_xtra_maturin_args[@]}"
+maturin build --release --strip --manylinux off "${_xtra_maturin_args[@]}"
 
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/vl_convert_python*.whl --no-deps -vv
