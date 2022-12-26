2f53a6dbadba5104832d5b027e9e9e3b4d5d44bf
Jon Mease <jonmmease@gmail.com>
2022-12-26 16:07:41 -0500

    Cross-comilation logic from orjson feedstock

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,29 @@
 declare -a _xtra_maturin_args
 
 if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] ; then
+
+      mkdir $SRC_DIR/.cargo
+      cat <<EOF >> $SRC_DIR/.cargo/config
+# Required for intermediate codegen stuff
+[target.x86_64-apple-darwin]
+linker = "$CC_FOR_BUILD"
+# Required for final binary artifacts for target
+[target.aarch64-apple-darwin]
+linker = "$CC"
+rustflags = [
+  "-C", "link-arg=-undefined",
+  "-C", "link-arg=dynamic_lookup",
+]
+
+EOF
+
   _xtra_maturin_args+=(--target=aarch64-apple-darwin)
+
+  # This variable must be set to the directory containing the target's libpython DSO
+  export PYO3_CROSS_LIB_DIR=$PREFIX/lib
+
+  # xref: https://github.com/PyO3/pyo3/commit/7beb2720
+  export PYO3_PYTHON_VERSION=${PY_VER}
 fi
 
 maturin build --release --strip --manylinux off "${_xtra_maturin_args[@]}"
