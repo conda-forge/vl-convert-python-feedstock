dcaeff37805206a32f0ed8736cb34c03ee87e7ab
Jon Mease <jonmmease@gmail.com>
2022-12-26 18:47:00 -0500

    Try V8_FROM_SOURCE for osx-arm build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,6 @@
-## Set conda CC as custom CC in Rust
-#export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
-#export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
-#export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
+if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] ; then
+  export V8_FROM_SOURCE=1
+fi
 
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}"
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/vl_convert_python*.whl --no-deps -vv
