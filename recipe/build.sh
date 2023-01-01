c0f7a7fc8237190fc95416ea39e283d957d3c2f8
Jon Mease <jonmmease@gmail.com>
2023-01-01 07:18:10 -0500

    Try override TARGET envar

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,17 @@
 ## Set conda CC as custom CC in Rust
 #export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=$CC
 #export CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER=$CC
-export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
+#export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER=$CC
+
+echo Original target is $TARGET
+
+if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] ; then
+  # Manually override TARGET, which rusty_v8 uses to determine which share libraries to
+  # link against
+  export TARGET = "aarch64-apple-darwin"
+fi
+
+echo Final target is $TARGET
 
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}"
 "${PYTHON}" -m pip install $SRC_DIR/target/wheels/vl_convert_python*.whl --no-deps -vv
