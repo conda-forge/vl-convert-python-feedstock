5313d7d54ec4ab3cb03ee020057bb2bd95f85ef7
Jon Mease <jonmmease@gmail.com>
2023-01-01 08:07:10 -0500

    Try orjson config again

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,9 +6,22 @@
 echo Original target is $TARGET
 
 if [ "$target_platform" = "osx-arm64" ] && [ "$CONDA_BUILD_CROSS_COMPILATION" = "1" ] ; then
-  # Manually override TARGET, which rusty_v8 uses to determine which share libraries to
-  # link against
-  export TARGET="aarch64-apple-darwin"
+  mkdir $SRC_DIR/.cargo
+  cat <<EOF >> $SRC_DIR/.cargo/config
+# Required for intermediate codegen stuff
+[target.x86_64-apple-darwin]
+linker = "$CC_FOR_BUILD"
+
+# Required for final binary artifacts for target
+[target.aarch64-apple-darwin]
+linker = "$CC"
+rustflags = [
+  "-C", "link-arg=-undefined",
+  "-C", "link-arg=dynamic_lookup",
+]
+
+EOF
+
   maturin build --release --target aarch64-apple-darwin --strip --manylinux off --interpreter="${PYTHON}"
 else
   maturin build --release --strip --manylinux off --interpreter="${PYTHON}"
