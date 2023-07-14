00eb719690e2160b857d6fdbe1c1180f8bf02a20
Jon Mease <jonmmease@gmail.com>
2023-07-14 10:53:44 -0500

    try again

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,4 @@
 ls -la
-python -m pip install ./*.whl -vv
+mv vl_convert_python-0.11.2-cp37-abi3-macosx_11_0_arm64.whl vl_convert_python-0.11.2-cp37-abi3-macosx_10_7_x86_64.whl
+python -m pip install vl_convert_python-0.11.2-cp37-abi3-macosx_10_7_x86_64.whl -vv
 echo "Pip install complete"
