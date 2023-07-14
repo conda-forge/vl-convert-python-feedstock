68b58c324505fa9b4b671a161b3b3951d1a6d2f3
Jon Mease <jonmmease@gmail.com>
2023-07-14 13:41:00 -0500

    try again

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
-cat conda_build.sh
-mv vl_convert_python-0.11.2-cp37-abi3-macosx_11_0_arm64.whl vl_convert_python-0.11.2-cp37-abi3-macosx_10_7_x86_64.whl
-ls -la
 python --version
+ls -la
+
+#mv vl_convert_python-0.11.2-cp37-abi3-macosx_11_0_arm64.whl vl_convert_python-0.11.2-cp37-abi3-macosx_10_7_x86_64.whl
 #pip install ./vl_convert_python-0.11.2-cp37-abi3-macosx_10_7_x86_64.whl -vv
-echo "Pip install complete"
+#echo "Pip install complete"
